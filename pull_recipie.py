2dd2c6ab1b5e1b4bc9c6b30e87b23f3b186a5728
tschmoog <bop_z@hotmail.co.uk>
2020-04-19 12:44:44 +0100

    Initial outline draft for pulling recipie from internet

---
+++ pull_recipie.py
@@ -0,0 +1,19 @@
+# This file gets the data for a recipie from a website (starting with BBC food)
+# and gets metadata
+
+# Ingredients
+# Timings
+# Costs
+# Calories
+
+
+# Step one: go to a site (BBC) 
+
+# Step two: At the site, pull relevant information
+
+# Two A: Pull Ingredient list
+
+# Two B: Pull time taken
+
+# Two C/D TO DO after MVP created as they are more complicated: Calories/Costs
+
