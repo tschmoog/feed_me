68c8eee2b07831a17ee8c909be0c0d29df18ba23
tschmoog <bop_z@hotmail.co.uk>
2020-04-28 22:04:29 +0100

    Revert "Started parsing the site for links."
    This reverts commit 522db72e49975b31e103db5439a3186909c374a5.

--- get_recipie_links.py
+++ get_recipie_links.py
@@ -3,17 +3,6 @@
 
 from bs4 import BeautifulSoup
 import csv
-from urllib.request import Request, urlopen
+import urllib.request
 
-req = Request("https://bbcgoodfood.com/recipes", headers={'user-agent': 'mozilla/5.0'})
-
-source = urlopen(req).read()
-soup = BeautifulSoup(source,'lxml')
-
-# parse page and print 'a' tags.
-for link in soup.findAll('a'):
-	print(link)
-
-
-# What does the webpage give us?
-
+source = urllib.request.urlopen("bbcgoodfood.com/recipies")
