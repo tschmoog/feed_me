4690a33e822805ee4cd14bdf536d475ca68000ee
tschmoog <bop_z@hotmail.co.uk>
2020-05-04 21:44:15 +0100

    Filtered links returned down to ones only concerned with the word "recipie"

--- get_recipie_links.py
+++ get_recipie_links.py
@@ -1,6 +1,5 @@
-# The purpose of this module is to crawl a homepage (BBC Good food) and 
-# find urls leading to recipies
-
+# The purpose of this module is to crawl a homepage (BBC Good food) and # find urls leading to recipies
+import re
 from bs4 import BeautifulSoup
 import csv
 from urllib.request import Request, urlopen
@@ -12,24 +11,17 @@ source= urlopen(req).read()
 # Put in a soup object so it can be parsed:
 soup = BeautifulSoup(source, features='html.parser') 
 
-# Trial output
-# Get all links on initial page
-for link in soup.find_all('a'):
-	print(link.get('href'))
+#print(soup.prettify())
 
+# Trial output
+
+# Get all recipie links on a page
+
+print(soup.find_all(href=re.compile("recipes")))
 # Now we must parse the above list to get links that lead to recipies.
 # Problem 1: Determine if a link is a recipie or not.
 # Problem 2: Given a link that DOESN'T lead to a recipie - should it be
 #            parsed in the hope that it may contain more recipie links?
 
-
-# Problem 1:
-# Inspect link:
-def is_link_recipie(link):
+def is_link_a_recipie():
     pass
-
-print("hungry? Y/N")
-x = input()
-print(x)
-
-
